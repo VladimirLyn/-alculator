878da1a0253eb6329bef10e08d08a4026511e31c
VladimirLynev <djdf112345@yandex.ru>
2022-03-03 23:41:35 +0300

    Добавьте файлы проекта.

---
+++ Сalculator/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Сalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+                Console.WriteLine("Введите первое число: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите второе число: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Введите номер операции: 1.Сложение 2.Вычитание 3.Умножение 4.Деление");
+
+                int operation = Convert.ToInt32(Console.ReadLine());
+                int result = 0;
+                switch (operation)
+                {
+                    case 1:
+                        result = num1 + num2;
+                        Console.WriteLine($"Результат операции {result}");
+                        break;
+
+                    case 2:
+                        result = num1 - num2;
+                        Console.WriteLine($"Результат операции {result}");
+                        break;
+
+                    case 3:
+                        result = num1 * num2;
+                        Console.WriteLine($"Результат операции {result}");
+                        break;
+
+                    case 4:
+                        result = num1 / num2;
+                        Console.WriteLine($"Результат операции {result}");
+                        break;
+
+                    default:
+                        Console.WriteLine("Неизвестная операция");
+                        break;
+                }
+            }
+        }
+    }
+}
